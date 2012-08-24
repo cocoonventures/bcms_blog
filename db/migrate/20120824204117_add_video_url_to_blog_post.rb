25768b334cce2334d3a859755868d508cd661172
Michael Madison <mkm@cocoonventures.com>
2012-08-24 14:35:58 -0700

    prefixing to bcms_blog

--- db/migrate/20120824204117_add_video_url_to_blog_post.rb
+++ db/migrate/20120824204117_add_video_url_to_blog_post.rb
@@ -1,8 +1,8 @@
 class AddVideoUrlToBlogPost < ActiveRecord::Migration
   def self.up
-    add_content_column :blog_posts, :video_url, :string
+    add_content_column :bcms_blog_blog_posts, :video_url, :string
   end  
   def self.down
-    remove_content_column :blog_posts, :video_url
+    remove_content_column :bcms_blog_blog_posts, :video_url
   end
 end
